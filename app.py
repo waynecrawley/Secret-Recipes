a037386d63499a19c78262a920cac601a89627e6
waynecrawley <enyawcrawley@gmail.com>
2020-06-28 21:51:13 +0000

    added secret key

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'secret_recipes'
-app.config["MONGO_URI"] = 'mongodb+srv://soolander:Waynelorisa@myfirstcluster-6szsu.mongodb.net/secret_recipes?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 mongo = PyMongo(app)
 
